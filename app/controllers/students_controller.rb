465767787cbc813d8fbb08fa56ba06ccc7803dc7
grant smith <grantsmith1256@gmail.com>
2019-03-26 01:46:29 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,22 @@
 class StudentsController < ApplicationController
-  
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
