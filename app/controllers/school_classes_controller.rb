b90139bae7eeeec5285259dca9f573a5a4cfca3f
github username <github email address>
2019-03-26 20:00:47 -0500

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,23 +3,36 @@ require 'pry'
 class SchoolClassesController < ApplicationController
 
   def index
-  end
-
-  def show
+    @classes = SchoolClass.all
   end
 
   def new
+    @class = SchoolClass.new
+  end
+
+  def show
+    @class = SchoolClass.find_by(params[:id])
   end
 
   def create
-    @school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
-    redirect_to school_class_path(@school_class)
+    @class = SchoolClass.create(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@class)
   end
 
   def edit
+    @class = SchoolClass.find_by(params[:id])
   end
 
   def update
+    @class = SchoolClass.find_by(params[:id])
+    @class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@class)
+  end
+
+  private
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
   end
 
 end
