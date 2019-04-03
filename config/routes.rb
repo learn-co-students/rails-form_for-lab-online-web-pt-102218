4809ae4a003a8182d66cc8b6d3ccd55194d6716c
github username <github email address>
2019-04-02 19:23:15 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :school_classes, :students, only: [:index, :new, :create, :edit, :update, :show]
+
+
+
 end
