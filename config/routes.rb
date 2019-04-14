106d607a9bdd91245931a16e78447b9ede9b0bb7
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 20:34:00 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :school_classes, only: [:index, :create, :new, :show, :edit, :update]
+  resources :students, only: [:index, :create, :new, :show, :edit, :update]
+  patch 'students/:id', to: 'students#update'
 end
